2b1cc9cf914a3a560b3c1d8ed36404b44642b1c0
Colin DeCarlo <colin@thedecarlos.ca>
2015-02-28 15:01:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Jank Profiler
+
+For when you don't want to figure out actual profilers.
+
+## Author
+
+Colin DeCarlo, colin@thedecarlos.ca
+
+## License
+
+Jank Profiler is licensed under the MIT License - see the LICENSE file for details
---
+++ src/JankProfiler.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * (c) Colin DeCarlo <colin@thedecarlos.ca>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+namespace JankProfiler;
+
+use JsonSerializable;
+
+class JankProfiler implements JsonSerializable
+{
+    protected $wrapped;
+    protected $calls;
+
+    public function __construct($classname = null, ...$constructorArgs)
+    {
+        if ($classname) {
+            $this->build($classname, ...$constructorArgs);
+        }
+    }
+
+    public function build($classname, ...$constructorArgs)
+    {
+        $this->reset();
+
+        $call = [
+            'type' => 'construct',
+            'meta' => [
+                'classname' => $classname,
+                'constructor_args' => $constructorArgs,
+            ],
+            'memory_before' => memory_get_usage(),
+            'start_time' => microtime(true),
+        ];
+
+        $obj = new $classname(...$constructorArgs);
+
+        $call['memory_after'] = memory_get_usage();
+        $call['end_time'] = microtime(true);
+
+        $this->wrapped = $obj;
+        $this->addCall($call);
+    }
+
+    protected function addCall($call)
+    {
+        $this->calls[] = $call;
+    }
+
+    public function report($format)
+    {
+        switch ($format) {
+            case 'array':
+                return $this->calls;
+                break;
+            case 'json':
+                return $this->jsonSerialize(JSON_PRETTY_PRINT);
+                break;
+            default:
+                return $this->jsonSerialize();
+        }
+    }
+
+    public function reset()
+    {
+        $this->calls = [];
+        $this->wrapped = null;
+    }
+
+    public function jsonSerialize($options = 0)
+    {
+        return json_encode($this->calls, $options);
+    }
+
+    public function __call($method, $args)
+    {
+        $call = [
+            'type' => 'method call',
+            'meta' => [
+                'method' => $method,
+                'arguments' => $args
+            ],
+            'start_time' => microtime(true),
+            'memory_before' => memory_get_usage()
+        ];
+
+        $result = $this->wrapped->$method(...$args);
+
+        $call['end_time'] = microtime(true);
+        $call['memory_after'] = memory_get_usage();
+
+        $this->addCall($call);
+
+        return $result;
+    }
+}
